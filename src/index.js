dfbf8da2a80241bb2086631decaa7225abf263cf
vibecode <rumashkin@gmail.com>
2017-04-14 23:13:50 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import InvoiceLineItems from './InvoiceLineItems';
+import './index.css';
+
+ReactDOM.render(
+  <InvoiceLineItems />,
+  document.getElementById('root')
+);
